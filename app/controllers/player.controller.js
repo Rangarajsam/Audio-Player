0ce379792f3a314f3daec108d142ea1b1f41c2fa
rangaraj <rangaraj.sam@gmail.com>
2016-11-12 21:11:23 +0530

    Playlist added

--- app/controllers/player.controller.js
+++ app/controllers/player.controller.js
@@ -1,5 +1,5 @@
 angular.module('playerController', [])
-    .controller('playerCtrl', ['$scope', 'playerControls', function ($scope, playerControls) {
+    .controller('playerCtrl', ['$scope', 'playerControls', '$interval', function ($scope, playerControls, $interval) {
 
         $scope.playerName = "Lissome";
         //$scope.song= new Audio("./songs/Malare_Ninne.mp3");
@@ -8,28 +8,67 @@ angular.module('playerController', [])
         $scope.song.autoplay = "";
         $scope.playing = false;
         $scope.song.selected = "";
-        $scope.songList=[];
+        $scope.song.duration = 0;
+        $scope.currentTime = 0;
+        $scope.songList = [];
+        $scope.currentSongIndex;
+        HTMLAudioElement.prototype.stop=function(){
+            this.pause=true;
+            this.currentTime=0.0;
+        };
         $scope.fileNameChanged = function (el) {
             $scope.localUrl = window.URL.createObjectURL(el.files[0]);
             console.log($scope.localUrl);
             $scope.selected = el.files[0].name;
             $scope.song = new Audio($scope.localUrl);
-            console.log($scope.selected);
-            $scope.songList.push($scope.song);
+            console.log($scope.song);
+            $scope.songList.push({
+                song: $scope.song,
+                songName: $scope.selected
+            });
+            $scope.$apply();
             console.log($scope.songList);
         };
         $scope.playAudio = function () {
-            console.log($scope.selected);
-            //var currentSong=$scope.song;
-            console.log($scope.song);
+             var totalSongs = $scope.songList.length;
+            var index =($scope.currentSongIndex) ? $scope.currentSongIndex : 0;
             if ($scope.playing) {
-                $scope.song.pause();
+                $scope.songList[index].song.pause();
+                $scope.currentSongIndex = index;
                 $scope.playing = false;
             } else {
-                $scope.song.play();
+                $scope.songList[index].song.play();
+                $scope.currentSongIndex = index;
+                $interval(function () {
+                    $scope.currentTime = $scope.songList[index].song.currentTime;
+                }, 1000);
                 $scope.playing = true;
             }
-            //$scope.playing=!$scope.playing;
-            console.log($scope.playing);
         };
-}]);
+        $scope.playFromPlaylist = function (index) {
+            var totalSongs = $scope.songList.length;
+            //var index =($scope.currentSongIndex) ? $scope.currentSongIndex : 0;
+            if ($scope.playing) {
+                $scope.songList[$scope.currentSongIndex].song.stop();
+                $scope.songList[index].song.play();
+                $scope.currentSongIndex = index;
+                $scope.playing = true;
+            } else {
+                $scope.songList[index].song.play();
+                $scope.currentSongIndex = index;
+                $interval(function () {
+                    $scope.currentTime = $scope.songList[index].song.currentTime;
+                }, 1000);
+                $scope.playing = true;
+            }
+
+        };
+
+}])
+    .filter('secToMins', function () {
+        return function (secs) {
+            var mins = parseInt(secs / 60, 10);
+            var osSecs = parseInt(secs % 60);
+            return mins + " : " + osSecs;
+        }
+    });
